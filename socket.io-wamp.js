366787152262c67ea3cd481020e58dc1f396fcba
Rafael Freitas <mail@rafael.to>
2016-08-12 11:16:49 -0300

    new version

--- socket.io-wamp.js
+++ socket.io-wamp.js
@@ -1,10 +1,10 @@
 /**
  * Socket.io WAMP - A simple client of PUBSUB and RPC interface for Socket.io server
  *
- * @author Rafael Freitas Lima
+ * @author Rafael Freitas Lima <rafael@tag.mx>
  */
 ;(function () {
-  var _export_socket_wamp = function (io) {
+  var _export_socket_wamp = function (socket) {
     /**
      * Promise Object Utility
      * @type {Object}
@@ -31,7 +31,12 @@
           if (self.status > -1) {
             setTimeout(function () {
               if (self.status === 1) {
-                callback(self.$data)
+                try {
+                  callback(self.$data)
+                } catch (e) {
+                  console.error(e)
+                  throw e
+                }
               }
             }, 0)
           }
@@ -48,7 +53,12 @@
           if (self.status > -1) {
             setTimeout(function () {
               if (self.status === 0) {
-                callback(self.$data)
+                try {
+                  callback(self.$data)
+                } catch (e) {
+                  console.error(e)
+                  throw e
+                }
               }
             }, 0)
           }
@@ -87,7 +97,9 @@
                 if (handlers.hasOwnProperty(i)) {
                   try {
                     handlers[i](data)
-                  } catch (e) {} finally {}
+                  } catch (e) {
+                    console.error(e)
+                  }
                 }
               }
             }, 0)
@@ -121,7 +133,7 @@
     }
 
     // export the promise interface
-    io.Q = q
+    // io.Q = q
 
     /**
      * Error Class
@@ -134,11 +146,14 @@
     }
     SocketWampError.prototype = Error.prototype
 
-    /**
-     * Copy of connect function of socket.io client
-     * @type {Function}
-     */
-    var io_connection_fn = io.connect
+    if (typeof socket !== 'undefined') {
+      create_wamp_interface(socket)
+    }
+
+    return {
+      q: q,
+      createSocketWampInterface: create_wamp_interface
+    }
 
     /**
      * Check if an object is a promise interface
@@ -157,57 +172,77 @@
       return Math.random().toString(36).slice(-8)
     }
 
-    // criando interface para captura da conexao
-
-    /**
-     * Wrap io connect function for apply the wamp interface
-     * @return {Object}
-     */
-    io.connect = function wrap_io_connect_function () {
-      return create_wamp_interface(io_connection_fn.apply(io, arguments))
-    }
-
     /**
      * Apply the interface for PUBSUB and RPC on socket client objects
      * @param  {SocketClient} socket Handle for client socket connections
      * @return {SocketClient}
      */
     function create_wamp_interface (socket) {
-      var rpc_pool = {}
-      var pubsub_pool = {}
+      if (socket.$wampInterface) {
+        return socket
+      }
+      /**
+       * Save RPC and Subscribe endpoints
+       * @type {Object}
+       */
+      var clientUriStorage = {}
 
-      /*
-          RPC
-          ====================================================================
-      */
+      socket.$wampInterface = true
 
-      function is_registred (procedure_name) {
-        return rpc_pool.hasOwnProperty(procedure_name)
+      socket.on('wamp_pack_client', wamp_pack)
+
+      function send (pack, done) {
+        pack = pack || {}
+        pack.caller = socket.id
+        socket.emit('wamp_pack_server', pack, function callback (tick) {
+          done(tick)
+        })
       }
 
-      /**
-       * Handle calls to forward results of calls for the right client
-       * @param  {Object} pack
-       * @param  {Function} callback
-       */
-      socket.on('wamp_call_callee', function (pack, callback) {
-        // checa se a procedure existe no pool
-        if (pack.procedure && is_registred(pack.procedure)) {
-          var procedure = rpc_pool[pack.procedure]
-          if (typeof procedure.procedure === 'function') {
-            var result = procedure.procedure.apply(procedure, pack.args)
-            if (isPromise(result)) {
-              result.then(callback)
+      function wamp_pack (pack, sendback) {
+        switch (pack.type) {
+          case 'remote_call':
+            if (typeof clientUriStorage[pack.uri] !== 'undefined') {
+              var result
+              try {
+                result = clientUriStorage[pack.uri].call(socket, pack.args, pack.kwargs, pack)
+                if (isPromise(result)) {
+                  result
+                  .then(function (res) {
+                    sendback(makeOk(res, 'wamp_call_ok'))
+                  })
+                  .catch(function (err) {
+                    sendback(makeError('Runtime error', 'wamp_error_runtime'), err)
+                  })
+                } else {
+                  sendback(makeOk(result, 'wamp_call_ok'))
+                }
+              } catch (e) {
+                sendback(makeError('Runtime error', 'wamp_error_runtime'), e)
+                throw e
+              }
             } else {
-              callback(result)
+              sendback(makeError('Procedure not registred on this client', 'wamp_error_register_remote'))
             }
-          } else {
-            // TODO levantar erro de procedure invalida
-          }
-        } else {
-          // TODO enviar erro de procedure nao configurada
+            break
+          case 'remote_publish':
+            if (typeof clientUriStorage[pack.uri] !== 'undefined') {
+              var result
+              try {
+                result = clientUriStorage[pack.uri].call(socket, pack.args, pack.kwargs, pack)
+                sendback(makeOk(result, 'wamp_publish_ok'))
+              } catch (e) {
+                sendback(makeError('Runtime error', 'wamp_error_runtime'), e)
+                throw e
+              }
+            } else {
+              sendback(makeOk('on client', 'wamp_publish_not_exists'))
+            }
+            break
+          default:
+
         }
-      })
+      }
 
       /**
        * Register a procedure on main socket server
@@ -215,7 +250,8 @@
        *
        * Usage 1:
        *
-       * 	socket.register("my.procedure.name", function(param1Val, param2Val){
+       * 	socket.register("my.procedure.name", function(args, kwargs, details){
+       * 		var param1Val = args[0], param2Val = args[2]
        * 		return param1Val + param2Val
        * 	})
        * 	.then(function(){
@@ -227,12 +263,12 @@
        *
        * Usage 2: with promises
        *
-       * 	socket.register("my.procedure.name", function(param1Val, param2Val){
+       * 	socket.register("my.procedure.name", function(args, kwargs, details){
        * 		var deferred = q.defer()
        *
        * 		// schedule for the future
        * 		setTimeout(function(){
-       * 			deferred.resolve(param1Val + param2Val)
+       * 			deferred.resolve(args[0] + args[1])
        * 		}, 1000)
        *
        * 		return deferred.promise
@@ -244,25 +280,67 @@
        * 		console.error("Something was wrong on register", err)
        * 	})
        *
-       * @param  {String}   procedure_name
-       * @param  {Function} fn
+       * @param  {URI}   procedure – the URI of the procedure to register
+       * @param  {Function} endpoint – the function that provides the procedure implementation
+       * @param  {object} optional - specifies options for registration (see below)
        * @return {Promise}
        */
-      socket.constructor.prototype.register = function register (procedure_name, fn) {
+      socket.register = function register (procedure, endpoint, options) {
         var dfd = q.defer()
 
-        socket.emit('wamp_register', procedure_name, function _wamp_register_callback (response) {
-          // console.log("wamp_register",response)
-          if (response.code === 'wamp_registred_success') {
-            rpc_pool[procedure_name] = {
-              name: procedure_name,
-              procedure: fn,
-              client: socket
-            }
-            dfd.resolve(response)
-          } else {
-            dfd.reject(response)
-          // throw response
+        send({
+          type: 'register',
+          uri: procedure,
+          options: options
+        }, function (tick) {
+          switch (tick.code) {
+            case 'wamp_register_ok':
+              clientUriStorage[procedure] = endpoint
+              dfd.resolve(tick)
+              break
+            case 'wamp_error_register':
+            default:
+              dfd.reject(tick)
+          }
+        })
+
+        return dfd.promise
+      }
+
+      /**
+       * Unregister a procedure on main socket server
+       * @example
+       *
+       * Usage:
+       *
+       * 	socket.unregister("my.procedure.name")
+       * 	.then(function(){
+       * 		console.log("my.procedure.name was unregistered")
+       * 	})
+       * 	.catch(function(err){
+       * 		console.error("Something was wrong on register", err)
+       * 	})
+       *
+       * @param  {URI}   procedure – the URI of the procedure to register
+       * @param  {Function} endpoint – the function that provides the procedure implementation
+       * @param  {object} optional - specifies options for registration (see below)
+       * @return {Promise}
+       */
+      socket.unregister = function unregister (procedure) {
+        var dfd = q.defer()
+
+        send({
+          type: 'unregister',
+          uri: procedure
+        }, function (tick) {
+          switch (tick.code) {
+            case 'wamp_unregister_ok':
+              delete clientUriStorage[procedure]
+              dfd.resolve(tick)
+              break
+            case 'wamp_error_unregister':
+            default:
+              dfd.reject(tick)
           }
         })
 
@@ -282,171 +360,164 @@
        * 	})
        *
        *
-       * @param  {String} procedure_name
-       * @param  {Mixed} args
+       * @param  {URI} procedure – the URI of the procedure to call
+       * @param  {Array} args - optional - call arguments
+       * @param  {Object} kwargs – optional - call arguments
+       * @param  {Object} options – optional - options for the call (see below)
        * @return {Promise}
        */
-      socket.constructor.prototype.call = function register (procedure_name) {
-        var args = Array.prototype.slice.call(arguments)
-        args.shift()
-
+      socket.call = function call (procedure, args, kwargs, options) {
         var dfd = q.defer()
 
-        socket.emit('wamp_call', {procedure: procedure_name, args: args}, function _wamp_call_callback (pack) {
-          if (!pack || pack.code === 'wamp_call_fail') {
-            // console.error("SocketWampError", pack)
-            dfd.reject(new SocketWampError(pack || 'Empty pack from server'))
-          } else {
-            dfd.resolve(pack)
+        if (!Array.isArray(args)) {
+          throw new Error('`args` needs to be an Array in call(procuedure {string}, args {Array}, kwargs {object}, options {object})')
+        }
+
+        send({
+          type: 'call',
+          uri: procedure,
+          args: args,
+          kwargs: kwargs,
+          options: options
+        }, function (tick) {
+          switch (tick.code) {
+            case 'wamp_call_ok':
+              // resolve with the remote call results
+              dfd.resolve(tick.data)
+              break
+            case 'wamp_error_runtime':
+            default:
+              dfd.reject(tick)
+          }
+        })
+
+        // socket.emit('wamp_call', {procedure: procedure_name, args: args}, function _wamp_call_callback (pack) {
+        //   if (!pack || pack.code === 'wamp_call_fail') {
+        //     // console.error("SocketWampError", pack)
+        //     dfd.reject(new SocketWampError(pack || 'Empty pack from server'))
+        //   } else {
+        //     dfd.resolve(pack)
+        //   }
+        // })
+
+        return dfd.promise
+      }
+
+      /**
+       * Make a call to a procedure on socket server
+       * @example
+       * Usage:
+       * session.publish('com.myapp.hello', ['Hello, world!'])
+       *
+       * session.publish('com.myapp.hello', [], { text: 'Hello, world' })
+       *
+       * @param  {URI} topic – the URI of the topic to call
+       * @param  {Array} args - optional - application event payload
+       * @param  {Object} kwargs – optional - application event payload
+       * @param  {Object} options – optional - specifies options for publication (see below)
+       * @return {Promise}
+       */
+      socket.publish = function publish (topic, args, kwargs, options) {
+        var dfd = q.defer()
+
+        if (!Array.isArray(args)) {
+          throw new Error('`args` needs to be an Array in publish(procuedure {string}, args {Array}, kwargs {object}, options {object})')
+        }
+
+        send({
+          type: 'publish',
+          uri: topic,
+          args: args,
+          kwargs: kwargs,
+          options: options
+        }, function (tick) {
+          switch (tick.code) {
+            case 'wamp_publish_not_exists':
+            case 'wamp_publish_ok':
+              // resolve with the remote call results
+              dfd.resolve(tick.data)
+              break
+            case 'wamp_error_runtime':
+            default:
+              dfd.reject(tick)
           }
         })
 
         return dfd.promise
       }
 
-      /*
-          PUBSUB
-          ====================================================================
-      */
-
-      /*
-          EVENT HANDLERS
-       */
-
-      socket.on('disconnect', function _wamp_connect () {
-        unregister_subscriber(socket)
-      })
-      socket.on('error', function _wamp_connect () {
-        unregister_subscriber(socket)
-      })
-
       /**
-       * Register a callback for a topic on a client
-       * @param  {String}         topic
-       * @param  {SocketClient}   client
-       * @param  {Function}       callback
-       */
-      function register_subscriber (topic, client, callback) {
-        if (typeof pubsub_pool[topic] === 'undefined') {
-          pubsub_pool[topic] = {
-            name: topic,
-            callbacks: [],
-            clients: []
-          }
-        }
-        callback.$client = client
-        pubsub_pool[topic].callbacks.push(callback)
-      }
-
-      /**
-       * Unregister a callback from pubsub pool
-       * @param  {SocketClient} client
-       */
-      function unregister_subscriber (client) {
-        for (var j in pubsub_pool) {
-          if (pubsub_pool.hasOwnProperty(j)) {
-            var route = pubsub_pool[j]
-            for (var i = route.callbacks.length - 1; i >= 0; i--) {
-              if (route.callbacks[i].$client === client) {
-                route.callbacks.splice(i, 1) // remove from array
-              }
-            }
-          }
-        }
-      }
-
-      /**
-       * EVENT
-       * Listening publish command for the client
-       * pack: {"route":"sample.topic","args":["sample param"]}
-       * @param  {Object} pack
-       */
-      socket.on('wamp_publish_callee', function _wamp_publish_callee (pack) {
-        if (pubsub_pool.hasOwnProperty(pack.route)) {
-          for (var i = 0; i < pubsub_pool[pack.route].callbacks.length; i++) {
-            try {
-              var fn = pubsub_pool[pack.route].callbacks[i]
-              fn.apply(socket, pack.args)
-            } catch (e) {} finally {}
-          }
-        }
-      })
-
-      /**
-       * Subscribe a top on main socket server
+       * Register a procedure on main socket server
        * @example
        *
-       * Usage:
+       * Usage 1:
        *
-       * 	socket.subscribe("my.topic.name", function(param1Val, param2Val){
-       * 		console.log("I listened it:", param1Val + param2Val)
+       * 	socket.subscribe("my.topic.name", function(args, kwargs, details){
+       * 		var param1Val = args[0], param2Val = args[2]
        * 	})
        * 	.then(function(){
        * 		console.log("my.topic.name is registred")
        * 	})
        * 	.catch(function(err){
-       * 		console.error("Something was wrong on subscribe", err)
+       * 		console.error("Something was wrong on register", err)
        * 	})
        *
-       *
-       * @param  {String}   topic
-       * @param  {Function} fn
+       * @param  {URI}   topic – the URI of the topic to register
+       * @param  {Function} endpoint – the function that provides the topic implementation
+       * @param  {object} optional - specifies options for registration (see below)
        * @return {Promise}
        */
-      socket.constructor.prototype.subscribe = function subscribe (topic, callback) {
+      socket.subscribe = function subscribe (topic, endpoint, options) {
         var dfd = q.defer()
 
-        socket.emit('wamp_subscribe', topic, function _wamp_subscribe_callback (response) {
-          // console.log("wamp_subscribe",response)
-          if (response.code === 'wamp_subscribe_success') {
-            register_subscriber(topic, socket, callback)
-
-            dfd.resolve(response)
-          } else {
-            dfd.reject(response)
-          // throw response
+        send({
+          type: 'subscribe',
+          uri: topic,
+          options: options
+        }, function (tick) {
+          switch (tick.code) {
+            case 'wamp_subscribe_ok':
+              clientUriStorage[topic] = endpoint
+              dfd.resolve(tick)
+              break
+            case 'wamp_error_subscribe':
+            default:
+              dfd.reject(tick)
           }
         })
 
         return dfd.promise
       }
+    }
 
-      /**
-       * Publish into a topic
-       *
-       * @example
-       * Usage:
-       * 	socket.publish("my.topic.name", "param1", "param2")
-       * 	.then(function(){
-       * 		console.log("my.topic.name was published")
-       * 	})
-       * 	.catch(function(err){
-       * 		console.error("Something was wrong", err)
-       * 	})
-       *
-       * @param  {String} topic
-       * @param  {Mixed} args
-       * @return {Promise}
-       */
-      socket.constructor.prototype.publish = function publish (topic) {
-        var args = Array.prototype.slice.call(arguments)
-        args.shift()
-
-        var dfd = q.defer()
-
-        socket.emit('wamp_publish', {route: topic, args: args}, function _wamp_publish_callback (pack) {
-          if (pack.code !== 'wamp_publish_ok') {
-            // console.error("SocketWampError", pack)
-            dfd.reject(new SocketWampError(pack))
-          } else {
-            dfd.resolve(pack)
-          }
-        })
-
-        return dfd.promise
+    /**
+     * Easy create WAMP response object
+     * @param  {String} data
+     * @param  {String} code
+     * @return {Object}
+     */
+    function makeOk (data, code) {
+      return {
+        code: code,
+        data: data
       }
+    }
 
-      return socket
+    /**
+     * Easy create an Error object
+     * @param  {String} msg  - error message
+     * @param  {String} code - code for wamp client or server decode
+     * @param  {Object | Array | String} details - details about the error like Exception native object
+     * @return {Error} - return a native object Error instance
+     */
+    function makeError (msg, code, details) {
+      var err = new Error(msg)
+      err.message = msg
+      err.code = code
+      if (typeof details !== 'undefined') {
+        err.details = details
+      }
+      return err
     }
   }
 
@@ -467,9 +538,9 @@
     } else {
       g = this
     }
-    g.wamplib = _export_socket_wamp
+    g.wampInterface = _export_socket_wamp
     if (typeof g.io === 'function') {
-      _export_socket_wamp(g.io)
+      // _export_socket_wamp()
     } else {
       console.error('Socket.io client is not present')
     }
